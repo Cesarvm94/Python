94f8fdbbbfb55f05572529abfbdf586d9015c834
César Villalobos <36650422+Cesarvm94@users.noreply.github.com>
2022-11-11 23:20:50 -0500

    Add files via upload

---
+++ Indice de masa corporal.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+# Created By  : César Villalobos
+# Created Date: 11/11/2022
+# Script que realiza el cálculo del índice de masa corporal solicitando datos por pantalla
+"""
+
+print(float(input("Ingrese su peso en kg: "))/pow(float(input("Ingrese su estatura en metros: ")),2))
---
+++ Saludo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 23:11:56 2022
+
+@author: cesar
+"""
+
+mitexto = "Hola mundo!"
+
+print(mitexto)
